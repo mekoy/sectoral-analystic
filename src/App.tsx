3a815e1559b763a18b4508a63d7ef565d92cda82
MEKO <yannmeko@gmail.com>
2023-03-03 19:29:45 +0100

    fix context chartjs give options custom

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,34 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "components/Layout";
 import DataVizAceptedYear20142019 from "pages/CurvesConsoAcepted2014-2019";
 import Header from "components/Header";
-import { Col } from "reactstrap";
+import {Col} from "reactstrap";
 
-import { ICurvesConsoContextType, useCurvesConsoContext } from "pages/context";
+import {ICurvesConsoContextType, useCurvesConsoContext} from "pages/context";
 import DataVizModule20142019 from "modules/DataVizConso-2014-2019";
 import DataVizConsumption2022 from "modules/DataVizConso-2022";
 import ChartsBySlider from "components/ChartsBySlider/ChartsBySlider";
 
 const App: React.FC<{}> = () => {
-  const { dataFetch }: ICurvesConsoContextType = useCurvesConsoContext();
-  return (
-    <Layout>
-      <Header>
-        <Col className="border w-100 py-4 text-xl-center">
-          <h1 className="fw-bold">
-            Analyse de la consommation de cet hiver 24-02-2023
-          </h1>
-        </Col>
-      </Header>
-      <ChartsBySlider>
-        <div>
-          <DataVizModule20142019 dataApi={dataFetch} />
-        </div>
-        <div>
-          <DataVizConsumption2022 dataApi={dataFetch} />
-        </div>
-      </ChartsBySlider>
-    </Layout>
-  );
+	const {dataFetch}: ICurvesConsoContextType = useCurvesConsoContext();
+	return (
+		<Layout>
+			<Header>
+				<Col className="border w-100 py-4 text-xl-center">
+					<h1 className="fw-bold">
+						Analyse de la consommation de cet hiver 24-02-2023
+					</h1>
+				</Col>
+			</Header>
+			<ChartsBySlider>
+				<div>
+					<DataVizModule20142019 dataApi={dataFetch} />
+				</div>
+				<div>
+					<DataVizConsumption2022 dataApi={dataFetch} />
+				</div>
+			</ChartsBySlider>
+		</Layout>
+	);
 };
 
 export default App;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,11 @@
+import {Scale, Chart} from "chart.js";
+import {ConfigOptions} from "./type";
+
+const DISPLAY = true;
+const BORDER = true;
+const CHART_AREA = false;
+const TICKS = true;
+
 export const labels = ["Oct", "Nov", "Dec", "Jan", "Fev", "Mars"];
 
 //get years for Data
@@ -10,3 +18,139 @@ export const yearsFull = (back: number): {label: string; year: number}[] => {
 		};
 	});
 };
+const plugin = {
+	id: "customCanvasBackgroundColor",
+	beforeDraw: (chart: Chart, args: any, options: {color: "#000"}) => {
+		const {ctx} = chart;
+		console.log(ctx, "ctx");
+		ctx.save();
+		ctx.globalCompositeOperation = "destination-over";
+		ctx.fillStyle = options.color || "#b65555";
+		ctx.fillRect(0, 0, chart.width, chart.height);
+		ctx.restore();
+	}
+};
+// chartJs config
+
+export const datavizConfig = [
+	{
+		options: {
+			responsive: true,
+			maintainAspectRatio: false,
+			onClick: (indexValue: any) => indexValue,
+			animations: {
+				x: {
+					duration: 0
+				},
+				tension: {
+					duration: 1000,
+					easing: "easeInSine",
+					from: 2,
+					to: 0.3,
+					loop: false
+				}
+			},
+			animation: {
+				onprogress: (context: {
+					chart: {};
+					initProgress: {value: 0};
+					progress: {value: number};
+					initial: boolean;
+					numSteps: number;
+					currentStep: number;
+				}) => {
+					let value = 15;
+					if (context.initial) {
+						value = context.currentStep / context.numSteps;
+					} else {
+						value = context.currentStep / context.numSteps;
+					}
+				}
+			},
+			interaction: {
+				mode: "point",
+				axis: "x",
+				intersect: false
+			},
+			layout: {
+				padding: {
+					left: 0,
+					right: 10,
+					top: 0,
+					bottom: 0
+				}
+			},
+			plugins: {
+				customCanvasBackgroundColor: {
+					color: "lightGreen"
+				},
+				events: ["click"],
+				title: {
+					display: true,
+					text: "Consommation attendu 2014 - 2019",
+					callbacks: {
+						label: (context: {}) => console.log(context, "context")
+					}
+				},
+				tooltip: {
+					callbacks: {
+						label: (context: ConfigOptions) => {
+							if (context.dataIndex === 0) {
+								return (context.label = "" + context.formattedValue);
+							}
+							if (context.dataIndex) {
+								return context.formattedValue;
+							}
+						}
+					}
+				},
+				legend: {
+					display: false,
+					usePointStyle: false,
+					labels: {}
+				},
+				formatter: {
+					callbacks: {
+						label: (context: any) => context
+					}
+				},
+				hover: {
+					mode: "index",
+					intersect: true,
+					footerFontStyle: "normal",
+					cursor: "pointer"
+				}
+			},
+			scale: {
+				x: {
+					grid: {
+						display: DISPLAY,
+						drawBorder: BORDER,
+						drawOnChartArea: CHART_AREA,
+						drawTicks: TICKS
+					},
+					gridLines: {
+						borderDash: [8, 4],
+						display: DISPLAY
+					},
+					linear: {
+						ticks: {
+							callback: {
+								label: (context: any) => context
+							}
+						}
+					}
+				},
+				y: {
+					grid: {
+						drawBorder: false
+					},
+					ticks: {
+						beginAtZero: true
+					}
+				}
+			}
+		},
+		plugins: [plugin]
+	}
+];
