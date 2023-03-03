d23532727404e1c362ab2ba13d69ab5c9933858c
MEKO <yannmeko@gmail.com>
2023-03-03 20:35:41 +0100

    fix rebase commit

--- src/components/ChartsBySlider/ChartsBySlider.tsx
+++ src/components/ChartsBySlider/ChartsBySlider.tsx
@@ -6,37 +6,37 @@ import "slick-carousel/slick/slick-theme.css";
 import "./ChartsBySlider.css";
 
 interface Chart {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 interface styleProps {
-  className?: string;
-  style?: {};
-  onClick?: () => void;
+	className?: string;
+	style?: {};
+	onClick?: () => void;
 }
 
-const NextArrow: React.FC<styleProps> = ({ style, className, onClick }) => {
-  return <div className={className} style={{ ...style }} onClick={onClick} />;
+const NextArrow: React.FC<styleProps> = ({style, className, onClick}) => {
+	return <div className={className} style={{...style}} onClick={onClick} />;
 };
 
-const PrevArrow: React.FC<styleProps> = ({ style, className, onClick }) => {
-  return <div className={className} style={{ ...style }} onClick={onClick} />;
+const PrevArrow: React.FC<styleProps> = ({style, className, onClick}) => {
+	return <div className={className} style={{...style}} onClick={onClick} />;
 };
 
-const ChartsBySlider: React.FC<Chart> = ({ children }) => {
-  const settings = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    initialSlide: 0,
-    fade: true,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-  };
+const ChartsBySlider: React.FC<Chart> = ({children}) => {
+	const settings = {
+		dots: false,
+		infinite: false,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		initialSlide: 0,
+		fade: true,
+		nextArrow: <NextArrow />,
+		prevArrow: <PrevArrow />
+	};
 
-  return <Slider {...settings}>{children}</Slider>;
+	return <Slider {...settings}>{children}</Slider>;
 };
 
 export default ChartsBySlider;
