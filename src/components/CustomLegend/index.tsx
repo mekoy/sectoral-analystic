8d9d9929c9fbeaec49961206d1af335de6edbe7d
MEKO <yannmeko@gmail.com>
2023-03-09 17:32:16 +0100

    update fix style

--- src/components/CustomLegend/index.tsx
+++ src/components/CustomLegend/index.tsx
@@ -1,67 +1,70 @@
-import { Chart, ChartData, ChartOptions, LegendOptions } from "chart.js";
-import React, { useState } from "react";
-import { MdOutlineRemoveRedEye } from "react-icons/md";
+import {Chart, ChartData, ChartOptions, LegendOptions} from "chart.js";
+import React, {useState} from "react";
+import {MdOutlineRemoveRedEye} from "react-icons/md";
 
 interface LegendItemProps {
-  label?: string;
-  color?: string;
-  line?: boolean;
+	label?: string;
+	color?: string;
+	line?: boolean;
 }
 
-const LegendItem: React.FC<LegendItemProps> = ({
-  label,
-  color,
-  line,
-}) => {
-  return (
-    <div className="legend__content--item">
-      <div
-        className={`${line ? "customLine" : "customEdge"}`}
-        style={{
-          backgroundColor: color,
-        }}
-      />
-      <div>{label}</div>
-      <div className="showChart"><MdOutlineRemoveRedEye/></div>
-    </div>
-  );
+const LegendItem: React.FC<LegendItemProps> = ({label, color, line}) => {
+	return (
+		<div className="legend__content--item">
+			<div
+				className={`${line ? "customLine" : "customEdge"}`}
+				style={{
+					backgroundColor: color
+				}}
+			/>
+			<div className="label_legend">{label}</div>
+			<div className="showChart">
+				<MdOutlineRemoveRedEye />
+			</div>
+		</div>
+	);
 };
 
 interface LegendProps {
-  title?: React.ReactNode;
-  data: ChartData;
-  line?: boolean;
-  legendOptions?: ChartOptions;
+	title?: React.ReactNode;
+	data: ChartData;
+	line?: boolean;
+	legendOptions?: ChartOptions;
 }
 
-const CustomLegend: React.FC<LegendProps> = ({ data, title, line, legendOptions }) => {
-  const [hiddenDatasets, setHiddenDatasets] = useState<string[]>([]);
+const CustomLegend: React.FC<LegendProps> = ({
+	data,
+	title,
+	line,
+	legendOptions
+}) => {
+	const [hiddenDatasets, setHiddenDatasets] = useState<string[]>([]);
 
-   const toggleVisibility = (label: string) => {
-    if (hiddenDatasets.includes(label)) {
-      setHiddenDatasets(hiddenDatasets.filter((l) => l !== label));
-    } else {
-      setHiddenDatasets([...hiddenDatasets, label]);
-    }
-  };
-  
-  const legendItems = data.datasets.map((dataset, index) => {
-    return (
-      <LegendItem
-        line={line}
-        key={index}
-        label={dataset.label}
-        color={dataset.borderColor as string}
-      />
-    );
-  });
+	const toggleVisibility = (label: string) => {
+		if (hiddenDatasets.includes(label)) {
+			setHiddenDatasets(hiddenDatasets.filter((l) => l !== label));
+		} else {
+			setHiddenDatasets([...hiddenDatasets, label]);
+		}
+	};
 
-  return (
-    <div className="legend__content">
-      <h4>{title}</h4>
-      {legendItems}
-    </div>
-  );
+	const legendItems = data.datasets.map((dataset, index) => {
+		return (
+			<LegendItem
+				line={line}
+				key={index}
+				label={dataset.label}
+				color={dataset.borderColor as string}
+			/>
+		);
+	});
+
+	return (
+		<div className="legend__content">
+			<h4>{title}</h4>
+			{legendItems}
+		</div>
+	);
 };
 
 export default CustomLegend;
--- src/components/LineChart/index.tsx
+++ src/components/LineChart/index.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-  Filler,
-  Chart,
-  ChartOptions,
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend,
+	Filler
 } from "chart.js";
-import { Line } from "react-chartjs-2";
+import {Line} from "react-chartjs-2";
 
 import CustomLegend from "../CustomLegend";
 
@@ -21,57 +20,58 @@ ChartJS.defaults;
 export const chart = ChartJS.defaults;
 
 type LineChartProps = {
-  title?: React.ReactNode;
-  line?: boolean;
-  customClass?: string;
-  width?: string | number;
-  height?: string | number;
-  data: IpPropsDataset;
-  options: {};
-  plugins?: any;
+	title?: React.ReactNode;
+	line?: boolean;
+	customClass?: string;
+	width?: string | number;
+	height?: string | number;
+	data: IpPropsDataset;
+	options: {};
+	plugins?: any;
 };
 export interface IpPropsDataset {
-  labels: string[];
-  datasets: {
-    label: string;
-    data: number[];
-    borderColor?: any;
-    backgroundColor?: any; // TODO
-    fill?: boolean | string | number | object;
-    opacity?: number;
-    hidden?: boolean;
-  }[];
+	labels: string[];
+	datasets: {
+		label: string;
+		data: number[];
+		borderColor?: any;
+		backgroundColor?: any; // TODO
+		fill?: boolean | string | number | object;
+		opacity?: number;
+		hidden?: boolean;
+	}[];
 }
 
 ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-  Filler
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend,
+	Filler
 );
 
 const LineChart: React.FC<LineChartProps> = ({
-  data,
-  options,
-  width,
-  height,
-  title,
-  line,
-  customClass,
+	data,
+	options,
+	width,
+	height,
+	title,
+	line,
+	customClass
 }) => {
-  //const chartRef = React.useRef<Chart>(null);
-  return (
-    <div className={customClass}>
-      <Line options={options} data={data} width={width} height={height} />
-      <div className="box legend">
-        <CustomLegend data={data} title={title} line={line} />
-      </div>
-    </div>
-  );
+	//const chartRef = React.useRef<Chart>(null);
+	return (
+		<div className={customClass}>
+			<Line options={options} data={data} width={width} height={height} />
+			<div className="box legend">
+				<CustomLegend data={data} title={title} line={line} />
+			</div>
+		</div>
+	);
 };
 
 export default LineChart;
